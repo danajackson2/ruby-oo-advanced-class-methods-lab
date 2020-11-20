07da0e03eaca3252867749f11009af03233353dc
danabliss <danajackson2@gmail.com>
2020-11-19 16:02:00 -0800

    || operator

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -7,7 +9,57 @@ class Song
   end
 
   def save
-    self.class.all << self
+    @@all << self            
   end
 
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = Song.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.find{|song|song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)    
+    self.find_by_name(name) || self.create_by_name(name)  
+  end
+
+  def self.alphabetical
+    self.all.sort_by{|song|song.name}
+  end
+
+  def self.new_from_filename(filename)
+    arr = filename.delete_suffix('.mp3').split(' - ')
+    song = Song.new
+    song.artist_name = arr[0]
+    song.name = arr[1]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    arr = filename.delete_suffix('.mp3').split(' - ')
+    song = Song.new
+    song.artist_name = arr[0]
+    song.name = arr[1]
+    song.save
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
 end
